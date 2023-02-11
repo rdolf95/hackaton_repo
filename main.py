795d0322cedbc425e404648f59e215c347c1745d
hyungsoon <rdolf95@naver.com>
2023-02-11 16:32:35 +0900

    login 화면 추가

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app = Flask(__name__, template_folder="template", static_folder="static")
 @app.route('/')
 def index():
 
-    return render_template("index.html")
+    return render_template("login.html")
 
 @app.route('/strategy')
 def strategy():
